54c5700df1626a3d0e832bfb0c2a3ef9e8ce14b2
Julian pettersen <darlion2301@gmail.com>
2020-05-26 20:37:29 +0200

    Add SMS button logic
    Sms button now works on android, should open sms app with pre-filled text

--- screens/ShareScreen.js
+++ screens/ShareScreen.js
@@ -6,41 +6,65 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Button,
+    Button,
+  Alert
 } from "react-native";
+import * as Linking from "expo-linking";
+import * as SMS from "expo-sms";
+
+
 
 class ShareScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (
-   <View style={styles.container}>
-      
-    <TouchableOpacity style={styles.addButton} title="Button" >
-      <Button class="button"
-       title="Messenger"
-        color="#00C6FF"
-        />
-    </TouchableOpacity>
+  state = {
+    VyMessage:
+      "Hei, trykk på denne linken:https://www.youtube.com/watch?v=dQw4w9WgXcQ for å se hvor jeg er på reisa",
+    phoneNum: "95839305",
+    smsAvailable: undefined,
+  };
 
-    <TouchableOpacity style={styles.addButton} title="Button" >
-      <Button class="button"
-        title="SMS"
-        color="#00957A"
-        />
-    </TouchableOpacity>
-
-    <TouchableOpacity  style={styles.addButton} title="Button" >
-      <Button class="button"
-        title="Mail"
-        color="#B6B6B6"
-        />
-    </TouchableOpacity>
-    
- </View> );
+  OpenSmS = async () => {
+    console.log("fgwaad");
+    const isAvailableAsync = await SMS.isAvailableAsync();
+    if (isAvailableAsync) {
+      this.setState({
+        smsAvailable: false,
+      });
+      const message = SMS.sendSMSAsync(
+        this.state.phoneNum,
+        this.state.VyMessage
+      );
+      console.log(isAvailableAsync);
+    } else {
     }
+    const message = SMS.sendSMSAsync(this.state.phoneNum, this.state.VyMessage);
+    console.log(message);
+  };
+
+  componentDidMount = async () => {};
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.addButton} title="Button">
+          <Button
+            class="button"
+            title="Messenger"
+            color="#00C6FF"
+            onPress={() => this.OpenSmS()}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.addButton} title="Button">
+          <Button class="button" title="SMS" color="#80046b" />
+          {!this.state.smsAvailable && <Text>hei</Text>}
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.addButton} title="Button">
+          <Button class="button" title="Mail" color="#ea4335" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
