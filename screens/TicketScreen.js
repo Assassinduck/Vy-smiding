207d0b409e098a88ca7b1855997b7e08cdd5aeb0
Julian pettersen <darlion2301@gmail.com>
2020-05-28 15:05:22 +0200

    remove comments

--- screens/TicketScreen.js
+++ screens/TicketScreen.js
@@ -71,7 +71,6 @@ class TicketScreen extends Component {
     this.setState({ notification: notification });
   };
 
-  // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/dashboard/notifications
   sendPushNotification = async () => {
     
 
@@ -84,16 +83,7 @@ class TicketScreen extends Component {
       channelId: "android",
       _displayInForeground: true,
     };
-    // const response = await fetch("https://exp.host/--/api/v2/push/send", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Accept-encoding": "gzip, deflate",
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(message),
-      
-    // });
+
     const schedulingOptions = {
       time: new Date().getTime() + (5000)
     };
